2c19b64d0c75085c6b8637e60ba420dee5a5eece
Matheus Kurianski <matheus1340@hotmail.com>
2019-04-14 22:48:06 -0300

    primeiro rascunho da dashboard

---
+++ pages/painel.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Sintese Game Jr.</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="/css/common.css">
+    <link rel="stylesheet" type="text/css" media="screen" href="/css/pages/painel.css">
+    <link href="https://unpkg.com/nes.css@2.1.0/css/nes.min.css" rel="stylesheet" />
+</head>
+<body>
+<div class="pagina">
+    <?php include '../res/components/navbar.php';?>
+    <div class="container">
+        <div class="dashboard">
+            <div class="profile">
+                <img src="https://cdn3.iconfinder.com/data/icons/user-icon-3-1/100/user_3_Artboard_1_copy_2-512.png" />
+                <p>
+                    Fulado de Tal
+                </p>
+            </div>
+            <ul>
+                <li class="ativo">Início</li>
+                <li>Conquistas</li>
+                <li>Meu Perfil</li>
+                <li>Sair</li>
+            </ul>
+        </div>
+        <div class="conteudo">
+
+        </div>
+    </div>
+</div>
+</body>
+</html>
