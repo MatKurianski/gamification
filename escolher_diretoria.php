6abf3ca26e988a37eca3214d3dca2d4efb6945b0
GuilhermeBalog <guibalog@gmail.com>
2019-04-17 16:18:57 -0300

    versão inical da seleção de diretorias

--- escolher_diretoria.php
+++ escolher_diretoria.php
@@ -7,6 +7,8 @@
 	<head>
 		<title></title>
 		<meta charset="utf-8">
+		<link rel="stylesheet" type="text/css" href="css/pages/guild.css">
+		<link rel="stylesheet" type="text/css" href="css/common.css">
 	</head>
 	<body>
 		<h1>Escolha a diretoria</h1>
@@ -14,7 +16,7 @@
 			$query = $sys->listarDiretorias();
 			if(!empty($query)){
 				while($diretoria = $query->fetch_object()){
-					echo "<a href='escolher_membro.php?cd=$diretoria->cd_diretoria'>";
+					echo "<a class='botao' href='escolher_membro.php?cd=$diretoria->cd_diretoria' style='background-color: $diretoria->ds_cor'>";
 					echo $diretoria->nm_diretoria;
 					echo "</a><br>";
 				}
