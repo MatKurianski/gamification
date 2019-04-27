bb2fa93a7f829a78f390d090d76f9c5fcc4eba87
Matheus Kurianski <matheus1340@hotmail.com>
2019-04-27 18:04:54 -0300

    [WIP] Tela adicionar regras e regras atuais

--- pages/admin/cadastrar_regra.php
+++ pages/admin/cadastrar_regra.php
@@ -1,9 +1,7 @@
 <?php 
     include '../../class/Sys.php'; 
     $sys = new Sys();
-    $dias = 5;
-    $membroCd = 1;
-    $periodo = $sys->gerarPeriodo($dias);
+    $regras = $sys->listarRegras();
 ?>
 <!DOCTYPE html>
 <html>
@@ -45,25 +43,70 @@
             <a onclick=toggleDashboard() id="toggle-sidebar">+</a>
         </div>
         <div class="conteudo">
-            <form class="form-regras">
-                <h3>
-                    Adicionar nova regra:
-                </h3>
-                <table>
-                    <tr>
-                        <th>Nome da Regra</th>
-                        <th class="pontos">Pontos</th>
-                    </tr>
-                    <tr> 
-                        <td>
-                            <input class="input-nome" placeholder="Escreva o nome da regra aqui"></input>
-                        </td>
-                        <td class="pontos">
-                            <input class="input-pontos" type="number"></input>
-                        </td>
-                    </tr>
-                </table>
-            </form>
+            <div class="regras">
+                <div class="lista-regras">
+                    <h3>
+                        Regras atuais
+                    </h3>
+                    <table>
+                        <tr>
+                            <th>
+                                Nome
+                            </th>
+                            <th>
+                                Pontos
+                            </th>
+                            <th>
+                                Ativa?
+                            </th>
+                        </tr>
+                        <tr>
+                            <?php 
+                                if(!empty($regras)) {
+                                    while($regra = $regras->fetch_object()) {
+                                        echo "<tr>";
+                                            echo "<td>";
+                                                echo $regra->ds_regra;
+                                            echo "</td>";
+                                            echo "<td>";
+                                                echo $regra->qt_pontos;
+                                            echo "</td>";
+                                            echo "<td>";
+                                                echo $regra->st_regra;
+                                            echo "</td>";
+                                        echo "</tr>";
+                                    }
+                                }
+                            ?>
+                        </tr>
+                    </table>
+                </div>
+                <hr />
+                <form id="form-regras">
+                    <h3 onclick="toggleAdcRegra()" class="texto-adicionar-regra">
+                        Adicionar nova regra
+                    </h3>
+                    <table id="tabela-adicionar-regra" class="oculto">
+                        <tr>
+                            <th>Nome da Regra</th>
+                            <th class="pontos">Pontos</th>
+                        </tr>
+                        <tr> 
+                            <td>
+                                <input class="input-nome" placeholder="Escreva o nome da regra aqui"></input>
+                            </td>
+                            <td class="pontos">
+                                <input class="input-pontos" type="number"></input>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td colspan=2>
+                                <a href="#" id="enviar" class="oculto">Enviar</a>
+                            </td>
+                        </tr>
+                    </table>
+                </form>
+            </div>
         </div>
     </div>
 
@@ -80,6 +123,12 @@
                 classes.add('fechado');
                 toggle_sidebar.text = '+';
             }
+        };
+
+        const adc_regra = document.getElementById('tabela-adicionar-regra');
+
+        const toggleAdcRegra = () => {
+            adc_regra.classList.remove("oculto");
         };
     </script>
 </body>
