0c1fc031cca297fedf3096fa634ac07209f71353
Matheus Kurianski <matheus1340@hotmail.com>
2019-04-29 18:38:35 -0300

    sidebar agora é um componente

--- pages/admin/cadastrar_diretoria.php
+++ pages/admin/cadastrar_diretoria.php
@@ -16,32 +16,7 @@
 </head>
 <body>
     <div class="container">
-        <div class="dashboard">
-            <div class="profile">
-                <img src="https://cdn3.iconfinder.com/data/icons/user-icon-3-1/100/user_3_Artboard_1_copy_2-512.png" />
-                <p>
-                    Fulado de Tal
-                </p>
-            </div>
-            <ul id="abas" class="fechado">
-                <li>
-                    <a class="aba" href="#">Início</a>
-                </li>
-                <li>
-                    <a class="aba" href="/pages/perfil/historico.php">Conquistas</a>
-                </li>
-                <li>
-                    <a class="aba" href="">Meu Perfil</a>
-                </li>
-                <li class="ativo">
-                    <a class="aba">Administração</a>
-                </li>
-                <li>
-                    <a class="aba" href="#">Sair</a>
-                </li>
-            </ul>
-            <a onclick=toggleDashboard() id="toggle-sidebar">+</a>
-        </div>
+        <?php include '../../res/components/sidebar.php'; ?>
         <div class="conteudo">
           <div class="area-diretoria">
               <table class="nova-tabela">
@@ -76,20 +51,6 @@
         </div>
     </div>
 
-    <script>
-        const toggle_sidebar = document.getElementById('toggle-sidebar');
-        const abas = document.getElementById('abas');
-        
-        const toggleDashboard = () => {
-            const classes = abas.classList;
-            if(classes.contains('fechado')) {
-                classes.remove('fechado');
-                toggle_sidebar.text = '-';
-            } else {
-                classes.add('fechado');
-                toggle_sidebar.text = '+';
-            }
-        };
-</script>
+
 </body>
 </html>
